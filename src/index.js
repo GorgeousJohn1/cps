6afbe6510e6d18881ef6420b2db21e671e70dae2
Ivan Voronzhev <gorgeousjohn1@gmail.com>
2024-05-07 20:44:39 +0500

    swiper-fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./index.html";
+import './swiper/swiper';
 import "./index.scss";
 
 import './blocks/blocks';
 
 
 import './showMore/showMore';
-import './swiper/swiper';
+
 import './modals/modals';
